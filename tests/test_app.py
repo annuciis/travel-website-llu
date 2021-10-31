24bb8a647ecfe5c40cea2ef1e061831e52b31e77
annuciis <aannuucciiss@gmail.com>
2021-10-31 20:11:14 +0200

    update tests to be executable

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,26 @@
-
 from selenium import webdriver
+import unittest
 
 from pages.title_page import TitlePage
 from pages.tours_page import ToursPage
-browser = webdriver.Chrome()
 
-browser.get('http://localhost:3000')
 
-#Title page
-title_page = TitlePage(driver=browser)
-title_page.explore_button.click()
-assert browser.current_url == 'http://localhost:3000/tours', "Didn't navigate to tours page"
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://localhost:3000')
 
-#Tour page
-tour_page = ToursPage(driver=browser)
-assert tour_page.tour.count() == 5
-browser.quit()
+    def test_title_page_go_to_tours(self):
+        self.title_page = TitlePage(driver=self.driver)
+        self.title_page.explore_button.click()
+        assert self.driver.current_url == 'http://localhost:3000/tours', "Didn't navigate to tours page"
+        self.tour_page = ToursPage(driver=self.driver)
+        assert self.tour_page.tour.count() == 5
+
+    def exit(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
 
